5f76c0058fd77e165891f63889d16f0459de92cc
leda159 <sssss0109165@gmail.com>
2022-10-31 17:53:13 +0900

    11

--- day1031/src/day1031/Week.java
+++
@@ -1,9 +0,0 @@
-package day1031;
-
-//자바클래스
-//열거타입은 한정된 값을 가지는 데이터 타입 => enum
-//계절(봄,여름,가을,겨울),색상
-//열거타입은 상수로 선언(대문자로 선언)
-public enum Week {
-	MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
-}
